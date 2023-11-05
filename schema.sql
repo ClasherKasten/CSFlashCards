ce688ae1cfbb5f809f682bf34c878188250e936d
MarioBarwuah2 <149949855+MarioBarwuah2@users.noreply.github.com>
2023-11-05 15:23:11 +0100

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,17 @@
+-- drop table if exists cards;
+create table if not exists cards (
+  id integer primary key autoincrement,
+  type tinyint not null, /* 1 for vocab, 2 for code */
+  front text not null,
+  back text not null,
+  known boolean default 0,
+  programming_language text 
+);
+
+create table if not exists tags (
+  id integer primary key autoincrement,
+  name text not null,
+  -- card_id integer,
+  -- foreign key (card_id) references cards(id)
+);
+
---
+++ users_schema.sql
@@ -0,0 +1,1 @@
+create table if not exists users(id integer primary key, username text, password text, color_scheme text, light_scheme integer);
