a6777a11f28e358a6c26d82c7810f53efdf0ec85
ClasherKasten <cfb1470@gmail.com>
2023-11-07 17:08:11 +0100

    backend...

--- data/users_schema.sql
+++ data/users_schema.sql
@@ -1,1 +1,7 @@
-create table if not exists users(id integer primary key, username text, password text, color_scheme text, light_scheme integer);
+create table if not exists users(
+    id integer primary key autoincrement,
+    username text,
+    password text,
+    color_scheme text,
+    light_scheme integer
+);
--- flash_cards.py
+++ flash_cards.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import enum
 import os
 import sqlite3
 
@@ -13,92 +12,113 @@ from flask import render_template
 from flask import request
 from flask import session
 from flask import url_for
+from flask_login import login_required
+from flask_login import login_user
+from flask_login import LoginManager
+from flask_login import UserMixin
 
 
+# set up flask app
 app = Flask(__name__)
-nameDB = 'None'
-pathDB = 'db'
+app.config['SECRET_KEY'] = 'development key'
+
+# set up Flask-Login
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+# store open db connections
+OPEN_CONNECTIONS = []
 
 
-class CardTypes(enum.Enum):
-    TEXT = 0
-    CODE = 1
-    MATH = 2
-
-
-def load_config():
-    app.config.update(
-        dict(
-            SECRET_KEY='development key',
-        ),
-    )
-
-
-def connect_db():
-    rv = sqlite3.connect('users.db')
+def connect_db(name: str) -> sqlite3.Connection:
+    rv = sqlite3.connect(name)
+    OPEN_CONNECTIONS.append(name)
     rv.row_factory = sqlite3.Row
     return rv
 
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+def get_db(name: str) -> sqlite3.Connection:
+    if not hasattr(g, name):
+        setattr(g, name, connect_db(name))
+    return getattr(g, name)
+
+
+def process_db_name(name: str, to_user: bool = False) -> str:
+    if to_user:
+        return name.replace(' ', '_')
+    else:
+        return name.replace('_', ' ')
+
+
+class User(UserMixin):
+    def __init__(self, id: int, name: str, password: str) -> None:
+        self.id = id
+        self.name = name
+        self.password = password
+
+    @classmethod
+    def get_user_by_name(cls, name: str) -> tuple[str, str]:
+        db = get_db('users.db')
+        user = db.execute(
+            'SELECT id, password FROM users WHERE username=?', (name,),
+        ).fetchone()
+        return cls(user[0], name, user[1])
+
+
+@login_manager.user_loader
+def load_user(user_name: str):
+    return User.get_user_by_name(user_name)
 
 
 @app.teardown_appcontext
 def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+    for connection in OPEN_CONNECTIONS:
+        getattr(g, connection).close()
 
 
-@app.route('/')
-def index():
-    if session.get('logged_in'):
-        return redirect(url_for('cards'))
-    else:
-        return redirect(url_for('login'))
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        user = User.get_user_by_name(request.form['username'])
+        if user is None or request.form['password'] != user.password:
+            error = 'Invalid username or password!'
+        else:
+            login_user(user)
+            flask.cookies['username'] = request.form['username']
+            return redirect(url_for('sets'))
+    return render_template('login.html', error=error)
 
 
 @app.route('/cards')
+@login_required
 def cards():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-
     query = '''
         SELECT *
-        FROM cards where tag_id = ?
+        FROM cards where tag_id=?
         ORDER BY id DESC
     '''
     db_path = flask.cookies.get('username')
-    db_name = flask.cookies.get('username')
-    tag_id = int(request.args.get('tag_id'))
-    db = sqlite3.connect(os.path.join(db_path, db_name))
-    c = db.cursor()
-    c.execute(query, (tag_id,))
-
-    return render_template(
-        'cards.html',
-        cards=cards,
-    )
+    db_name = flask.cookies.get('current_set')
+    tag_id = int(request.args.get('current_tag_id'))
+    flask.cookies['current_tag_id'] = tag_id
+    db = get_db(f'{db_path}/{process_db_name(db_name)}.db')
+    cards = db.execute(query, (tag_id,)).fetchall()
+    return render_template('cards.html', cards=cards)
 
 
 @app.route('/cards/add', methods=['POST'])
+@login_required
 def add_card():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
+    # TODO: consider programming_language?
     db_path = flask.cookies.get('username')
-    db_name = flask.cookies.get('username')
-    tag_id = flask.cookies.get('tag_id')
-    db = sqlite3.connect(os.path.join(db_path, db_name))
-    c = db.cursor()
-    c.execute(
-        '''insert into cards (type, front, back, known, tag_id)
-            values (?,?,?,?,?)''',
+    db_name = flask.cookies.get('current_set')
+    tag_id = int(flask.cookies.get('current_tag_id'))
+    db = get_db(f'{db_path}/{process_db_name(db_name)}.db')
+    db.execute(
+        '''INSERT INTO cards (type, front, back, known, tag_id)
+            VALUES (?,?,?,?,?)''',
         (
             request.form['type'], request.form['front'],
             request.form['back'], request.form['known'], tag_id,
@@ -110,9 +130,8 @@ def add_card():
 
 
 @app.route('/cards/edit', methods=['POST'])
+@login_required
 def edit_card():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     command = '''
         UPDATE cards
         SET
@@ -123,8 +142,8 @@ def edit_card():
         WHERE id = ?
     '''
     db_path = flask.cookies.get('username')
-    db_name = flask.cookies.get('username')
-    db = sqlite3.connect(os.path.join(db_path, db_name))
+    db_name = flask.cookies.get('current_set')
+    db = get_db(f'{db_path}/{process_db_name(db_name)}.db')
     db.execute(
         command,
         [
@@ -141,12 +160,13 @@ def edit_card():
 
 
 @app.route('/cards/delete', methods=['POST'])
+@login_required
 def delete(card_id):
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     db_path = flask.cookies.get('username')
     db_name = flask.cookies.get('username')
-    db = sqlite3.connect(os.path.join(db_path, db_name))
+    db = get_db(f'{db_path}/{db_name}.db')
     db.execute('DELETE FROM cards WHERE id = ?', [card_id])
     db.commit()
     flash('Card deleted.')
@@ -154,12 +174,12 @@ def delete(card_id):
 
 
 @app.route('/learn')
+@login_required
 def memorize():
-
     tag_id = flask.cookies.get('tag_id')
     db_path = flask.cookies.get('username')
     db_name = flask.cookies.get('username')
-    db = sqlite3.connect(os.path.join(db_path, db_name))
+    db = get_db(f'{db_path}/{db_name}.db')
     cards = db.execute(
         'SELECT front,back,type FROM cards WHERE tag_id = ?', [
             tag_id,
@@ -170,45 +190,20 @@ def memorize():
 
 
 @app.route('/learned', methods=['POST'])
+@login_required
 def mark_known():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     db_path = flask.cookies.get('username')
     db_name = flask.cookies.get('username')
     card_id = int(request.form['card_id'])
-    db = sqlite3.connect(os.path.join(db_path, db_name))
+    db = get_db(f'{db_path}/{db_name}.db')
     db.execute('UPDATE cards SET known = 1 WHERE id = ?', [card_id])
     db.commit()
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    userdata = g.sqlite_db.execute(
-        'select username, password from users where username =?', (
-            request.form['username'],
-        ),
-    ).fetchone()
-    error = None
-    if request.method == 'POST':
-        if (
-                userdata is None
-                or request.form['password'] != userdata[1]
-        ):
-            error = 'Invalid username or password!'
-        else:
-            session['logged_in'] = True
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash("You've logged out")
-    return redirect(url_for('index'))
-
-
 @app.route('/sets')
+@login_required
 def sets():
     db_path = flask.cookies.get('username')
     dbs = [
@@ -220,6 +215,7 @@ def sets():
 
 
 @app.route('/sets/add', methods=['POST'])
+@login_required
 def add_set():
     db_path = flask.cookies.get('username')
     db_name = request.form['name']
@@ -227,6 +223,7 @@ def add_set():
 
 
 @app.route('/sets/delete', methods=['DELETE'])
+@login_required
 def delete_set():
     db_path = flask.cookies.get('username')
     db_name = request.form['name']
@@ -237,6 +234,7 @@ def delete_set():
 
 
 @app.route('/sets/edit', methods=['PATCH'])
+@login_required
 def edit_set():
     db_path = flask.cookies.get('username')
     old_db_name = request.form['old_name']
@@ -251,22 +249,23 @@ def edit_set():
 
 
 @app.route('/sets/<name>')
+@login_required
 def set_overview(name):
     db_path = flask.cookies.get('username')
     db_name = name + '.db'
-    db = sqlite3.connect(os.path.join(db_path, db_name))
-    c = db.cursor()
-    tags = c.execute('SELECT id, name FROM tags').fetchall()
+    flask.cookies['current_set'] = db_name
+    db = get_db(f'{db_path}/{db_name}.db')
+    tags = db.execute('SELECT id, name FROM tags').fetchall()
     return render_template('set_overview.html', tags=tags)
 
 
 @app.route('/tags/add', methods=['POST'])
+@login_required
 def add_tag():
     db_path = flask.cookies.get('username')
     db_name = flask.cookies.get('current_set')
-    db = sqlite3.connect(os.path.join(db_path, db_name))
-    c = db.cursor()
-    c.execute(
+    db = get_db(f'{db_path}/{db_name}.db')
+    db.execute(
         'INSERT INTO tags (tagName) VALUES (?)',
         [request.form['tagName']],
     )
@@ -274,22 +273,22 @@ def add_tag():
 
 
 @app.route('/tags/delete', methods=['DELETE'])
+@login_required
 def delete_tag():
     db_path = flask.cookies.get('username')
     db_name = flask.cookies.get('current_set')
-    db = sqlite3.connect(os.path.join(db_path, db_name))
-    c = db.cursor()
-    c.execute('Delete from tags where id =?', [request.form['tag_id']])
+    db = get_db(f'{db_path}/{db_name}.db')
+    db.execute('Delete from tags where id =?', [request.form['tag_id']])
     db.commit()
 
 
 @app.route('/tags/edit', methods=['PATCH'])
+@login_required
 def edit_tag():
     db_path = flask.cookies.get('username')
     db_name = flask.cookies.get('current_set')
-    db = sqlite3.connect(os.path.join(db_path, db_name))
-    c = db.cursor()
-    c.execute(
+    db = get_db(f'{db_path}/{db_name}.db')
+    db.execute(
         'Update tags set name =? where id = ?', [
             request.form['tagName'], request.form['tag_id'],
         ],
@@ -298,5 +297,4 @@ def edit_tag():
 
 
 if __name__ == '__main__':
-    load_config()
     app.run(host='0.0.0.0', debug=True)
